aece3fb1befd14bf5db84c6c80dbc543617d48cb
Mateo Sebastian Sotomayor Benavides <mateo.sotomayor@udla.edu.ec>
2025-01-21 07:31:29 -0500

    Agregar Carpetas MVVM

---
+++ ExamenMateoSotomayor/Models/Pelicula.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenMateoSotomayor.Models
+{
+    public class Pelicula
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Actor { get; set; }
+        public string Awards { get; set; }
+        public string Website { get; set; }
+        public string MateoSotomayor { get; set; }
+    }
+}
+

0d62170c68a7c2052f6f216d82bbfa7160345de8
Mateo Sebastian Sotomayor Benavides <mateo.sotomayor@udla.edu.ec>
2025-01-21 12:51:48 -0500

    Modificar Models-Basededatos

--- ExamenMateoSotomayor/Models/Pelicula.cs
+++ ExamenMateoSotomayor/Models/Pelicula.cs
@@ -1,13 +1,16 @@
-﻿namespace ExamenMateoSotomayor.Models
+﻿using SQLite;
+
+namespace ExamenMateoSotomayor.Models
 {
     public class Pelicula
     {
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Genre { get; set; } 
-        public string Actors { get; set; } 
+        public string Genre { get; set; }
+        public string Actors { get; set; }
         public string Awards { get; set; }
         public string Website { get; set; }
-        public string MateoSotomayor { get; set; } 
+        public string Descripcion { get; set; }
     }
 }
